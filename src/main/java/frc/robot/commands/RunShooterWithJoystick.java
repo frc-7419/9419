d64b08260af7a7adf299bb15d8fb89f6afb917aa
F.H <franklinhu2007@gmail.com>
2022-11-03 11:14:14 -0700

    Update RunShooterWithJoystick.java

--- src/main/java/frc/robot/commands/RunShooterWithJoystick.java
+++ src/main/java/frc/robot/commands/RunShooterWithJoystick.java
@@ -26,7 +26,7 @@ public class RunShooterWithJoystick extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooterSubsystem.coast();
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.

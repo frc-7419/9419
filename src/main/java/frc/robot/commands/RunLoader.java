b4368f8beba4f02cd26bda4815755027e1e9997d
d3vmeh <devxmehra@gmail.com>
2022-11-10 16:58:35 -0800

    Created a loader and intake command that doesn't use joystick and worked on TwoBallAuto

---
+++ src/main/java/frc/robot/commands/RunLoader.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LoaderSubsystem;
+
+public class RunLoader extends CommandBase {
+  /** Creates a new RunLoader. */
+  private LoaderSubsystem loaderSubsystem;
+  private double power;
+
+  public RunLoader(LoaderSubsystem loaderSubsystem, double power) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.loaderSubsystem = loaderSubsystem;
+    this.power = power;
+    addRequirements(loaderSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    loaderSubsystem.setPower(power);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    loaderSubsystem.setPower(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

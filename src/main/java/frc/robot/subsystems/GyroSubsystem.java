48fef594a9c90d2865c4a5c3081c70e7c7c90c2b
QLS7419TechSupport <qls7419leadership@gmail.com>
2022-11-18 16:52:16 -0800

    pigeon
    Co-Authored-By: rqhu2025 <91698182+rqhu2025@users.noreply.github.com>
    Co-Authored-By: Stephen Xie <49465499+StephenXie@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -9,29 +9,49 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.sensors.PigeonIMU;
 
 public class GyroSubsystem extends SubsystemBase {
   /** Creates a new GyroSubsystem. */
 
-  private AHRS ahrs;
+  private PigeonIMU gyro;
 
   public GyroSubsystem() {
-    try {
-			/* Communicate w/navX-MXP via the MXP SPI Bus (use mini USB to USB A cable)   
-			   Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or S     
-			   See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
-               ahrs = new AHRS(SerialPort.Port.kMXP); 
-		} catch (RuntimeException ex ) {
-            DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true); 
-        }
+    // try {
+		// 	/* Communicate w/navX-MXP via the MXP SPI Bus (use mini USB to USB A cable)   
+		// 	   Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or S     
+		// 	   See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
+    //            ahrs = new AHRS(SerialPort.Port.kMXP); 
+		// } catch (RuntimeException ex ) {
+    //         DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true); 
+    //     }
+    this.gyro = new PigeonIMU(0);
   }
 
-  public double getGyroAngle() {
-    return ahrs.getAngle();
+  public double getGyroYaw() {
+    double[] ypr = new double[3];
+    gyro.getYawPitchRoll(ypr);
+    return ypr[0];
+  }
+
+  public double getGyroPitch() {
+    double[] ypr = new double[3];
+    gyro.getYawPitchRoll(ypr);
+    return ypr[1];
+  }
+
+  public double getGyroRoll() {
+    double[] ypr = new double[3];
+    gyro.getYawPitchRoll(ypr);
+    return ypr[2];
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("gyro yaw", getGyroYaw());
+    SmartDashboard.putNumber("gyro pitch", getGyroPitch());
+    SmartDashboard.putNumber("gyro roll", getGyroRoll());
     // This method will be called once per scheduler run
   }
 }
